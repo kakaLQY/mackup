b41479be91a69b18a8922f6877a38f852aead4fb
Kaka <kaka@gini.co>
2020-01-30 17:34:32 +0800

    Updated to ignore not serious problem

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-fenv . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fenv . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' 2> /dev/null
 eval (direnv hook fish)
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/variables.fish
+
