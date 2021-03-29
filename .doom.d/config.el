12f8017c98bc441b245224d085adf536c6e2fce7
Kaka LI <drkaka@drkaka.com>
2021-03-29 17:54:40 +0800

    Added conf files and org-src-lang-modes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -11,6 +11,19 @@
 
 (setq-default fill-column 100)
 
+;; conf
+(add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.timer\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.target\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.mount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.slice\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.socket\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.path\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.netdev\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.network\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.link\\'" . conf-unix-mode))
+
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
 ;;
@@ -47,9 +60,14 @@
 (setq org-journal-date-format "%Y-%m-%d, %A")
 (setq org-journal-file-type 'monthly)
 
+(after! org-src
+  (add-to-list 'org-src-lang-modes '("bb" . clojure))
+  (add-to-list 'org-src-lang-modes '("conf-unix" . conf-unix)))
+
 (use-package! org-roam
   :config
-  (setq org-roam-directory (concat org-directory "/roam/")))
+  (setq org-roam-directory (concat org-directory "/roam/"))
+  (setq org-roam-tag-sources '(prop vanilla)))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
