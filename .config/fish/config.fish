124d6512273b19f238b87b673c7dcfdfba593e1c
Kaka LI <drkaka@drkaka.com>
2021-04-08 11:21:56 +0800

    Used Org to generate some of fish config files

--- .config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-fenv . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' 2> /dev/null
-eval (direnv hook fish)
-source ~/.config/fish/aliases.fish
-source ~/.config/fish/variables.fish
-source ~/.config/fish/private.fish
-
