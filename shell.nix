7301cd8f571065b41201877a89da73b5cef06fe1
Kaka LI <drkaka@drkaka.com>
2023-06-27 12:30:40 +0800

    Added keyboard OP and setting

---
+++ shell.nix
@@ -0,0 +1,11 @@
+with import (builtins.fetchGit {
+  url = "https://github.com/NixOS/nixpkgs.git";
+  ref = "master";
+  rev = "a5c77f8b933cd3e14f31ebec57ef080d894b6fac";
+}) {};
+
+mkShell {
+  buildInputs = [
+    babashka qmk
+  ];
+}
