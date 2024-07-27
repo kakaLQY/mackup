6791a6944fa1b01ab6647ccb19397822244710b1
Kaka LI <drkaka@drkaka.com>
2024-07-27 09:43:44 +0800

    Added anytype app

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 with import (builtins.fetchGit {
   url = "https://github.com/NixOS/nixpkgs.git";
   ref = "master";
-  rev = "6e62521155cd3b4cdf6b49ecacf63db2a0cacc73";
+  rev = "00fc3cbf8c21a4139245e1b9c6173ecf000b5c7f";
 }) {};
 
 mkShell {
   buildInputs = [
-    babashka nixfmt qmk ventoy-full via vial
+    anytype babashka nixfmt qmk ventoy-full via vial
   ];
 }
